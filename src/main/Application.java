d4ebebeb197b0a36ae97c97747af8c0d0aac8df5
Anton <antondanik32@mail.ru>
2022-10-22 15:58:50 +0300

    Fix main method and naming of xml file

--- src/main/Application.java
+++ src/main/Application.java
@@ -1,7 +1,11 @@
 package main;
 
+import main.service.ApplianceService;
+
 public class Application {
     public static void main(String[] args) {
-
+        ApplianceService applianceService = new ApplianceService();
+        System.out.println(applianceService.getCheapestAppliance());
+        System.out.println(applianceService.getKettles());
     }
 }
