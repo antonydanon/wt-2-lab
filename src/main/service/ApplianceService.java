dfb2d2abeafd2ded7369b7f5c151bb27e3cff455
Anton <antondanik32@mail.ru>
2022-10-22 15:53:23 +0300

    Implement service

--- src/main/service/ApplianceService.java
+++ src/main/service/ApplianceService.java
@@ -1,4 +1,23 @@
 package main.service;
 
+import main.dao.ApplianceDAO;
+import main.model.Appliance;
+import main.model.Kettle;
+
+import java.util.List;
+
 public class ApplianceService {
+    private final ApplianceDAO applianceDAO;
+
+    public ApplianceService() {
+        this.applianceDAO = new ApplianceDAO();
+    }
+
+    public Appliance getCheapestAppliance() {
+        return applianceDAO.findCheapestAppliance();
+    }
+
+    public List<Kettle> getKettles() {
+        return applianceDAO.findKettles();
+    }
 }
